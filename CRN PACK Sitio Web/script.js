2185fe84de94c02dbfcd9d57c9d9906d0f1d47ed
Macbeth Adolfo Carrillo Ibarra <31285010+MacbethCarrillo@users.noreply.github.com>
2024-09-23 20:35:21 -0700

    Update script.js

--- CRN PACK Sitio Web/script.js
+++ CRN PACK Sitio Web/script.js
@@ -48,3 +48,23 @@ function initCarousel() {
 }
 
 initCarousel();
+
+document.addEventListener('DOMContentLoaded', function () {
+    const selectElement = document.getElementById('sucursales-select');
+    const mapaSucursal = document.getElementById('mapa-sucursal');
+
+    // Establecer el valor por defecto (Culiacán)
+    selectElement.value = 'culiacan';
+
+    // Cambiar el mapa basado en la selección del combobox
+    selectElement.addEventListener('change', function () {
+        if (this.value === 'culiacan') {
+            mapaSucursal.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d879.5239729484308!2d-107.39984756060244!3d24.823387427683482!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86bcdb006754c6ad%3A0x1c781579c7476a81!2sCRN%20PACK%20CULIACAN!5e0!3m2!1ses-419!2smx!4v1726946954633!5m2!1ses-419!2smx";
+        } else if (this.value === 'guasave') {
+            mapaSucursal.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3599.340689200385!2d-108.47662082552945!3d25.56032981689846!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86bbbf001b7420a1%3A0xea6d4789764bdc4c!2sCRN%20PACK%20GUASAVE!5e0!3m2!1ses-419!2smx!4v1726947587768!5m2!1ses-419!2smx";
+        }
+    });
+});
+
+
+
